a20df9be72ab8ca309e2935ebcd5195e3441bbf4
whysnakes <88395238+whysnakes@users.noreply.github.com>
2021-08-10 16:39:12 +0800

    Added basic app structure
    Added divs for title, options, results and version

--- src/js/App.js
+++ src/js/App.js
@@ -1,21 +1,16 @@
 import '../css/App.css';
 import bg from '../static/backgrounds/btd6_background.png'
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div classname="Title"></div>
+      <div classname="Options"></div>
+      <div classname="Results"></div>
+      <footer>
+        <div classname="Version"></div>
+      </footer>
     </div>
   );
 }
