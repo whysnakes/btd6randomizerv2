f08a7446736c2071218e7adf4b0530d72d431b44
whysnakes <88395238+whysnakes@users.noreply.github.com>
2021-08-10 16:24:14 +0800

    Added background link and removed logo
    The logo is from the default test site

--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
+import '../css/App.css';
+import bg from '../static/backgrounds/btd6_background.png'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
