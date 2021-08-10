8b19142847b1e8dbdc913849b271b3cc2133405a
maquarocket <76931815+maquarocket@users.noreply.github.com>
2021-08-10 17:46:56 +0800

    Minor refactor and added background.
    Changed all instances of "class" to "className".
    Also removed bg import as background image is in custom.css.

--- src/js/App.js
+++ src/js/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import '../css/App.css';
 import '../css/custom.css';
 import * as BTD6Data from '../js/btd6info.js';
-import bg from '../static/backgrounds/btd6_background.png'
+
 
 function Title() {
   return (
-    <div class="transbox">
-			<h2 class="text-center btd6font">Bloons TD6 Randomizer</h2>
-			<h4 class="text-center btd6font">Randomizes options for maps, hero selection, mode selection and tower selection</h4>
+    <div className="transbox">
+			<h2 className="text-center btd6font">Bloons TD6 Randomizer</h2>
+			<h4 className="text-center btd6font">Randomizes options for maps, hero selection, mode selection and tower selection</h4>
 		</div>	
   );
 }
@@ -27,8 +27,8 @@ function Results() {
 
 function Version() {
   return (
-    <div class="transbox w-100 fixed-bottom text-end">
-				<a href="../../CHANGELOG.md" id="pagefooter" class="cleanlink mx-2">
+    <div className="transbox w-100 fixed-bottom text-end">
+				<a href="../../CHANGELOG.md" id="pagefooter" className="cleanlink mx-2">
           Version <strong>{BTD6Data.randomizer_version}</strong> based on BTD6 version <strong>{BTD6Data.btd6_version}</strong>
         </a>
 		</div>
@@ -37,7 +37,7 @@ function Version() {
 
 function App() {
   return (
-    <div className="App">
+    <div className="App background">
       <div className="Title">
         <Title />
       </div>
