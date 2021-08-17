52d4e909b2a6e3ced8c126c4be88805456919cdc
maquarocket <76931815+maquarocket@users.noreply.github.com>
2021-08-17 22:52:03 +0800

    Documented functions in btd6randomizer.js.
    Documentation standard is JSDoc 3.

--- src/js/btd6randomizer.js
+++ src/js/btd6randomizer.js
@@ -2,11 +2,21 @@ import * as BTD6Data from './btd6info';
 import * as random from './random';
 
 
+/**
+ * Returns a random BTD6 map.
+ * @param  None
+ * @return {String}     - Name of a random map.
+ */
 function getRandomMap() {
     const difficulty = random.getRandomObjectKey(BTD6Data.btd6_maps_by_difficulty_object);
     return random.getRandomObjectKey(BTD6Data.btd6_maps_by_difficulty_object[difficulty]);
 }
 
+/**
+ * Returns a random BTD6 game mode.
+ * @param  None
+ * @return {String}     - Name of a random game mode.
+ */
 function getRandomMode(isTowerTypeRestricted) {
     let modeArray = [];
     for(var i = 0; i < BTD6Data.btd6_modes.length; i++) {
@@ -17,10 +27,21 @@ function getRandomMode(isTowerTypeRestricted) {
     return random.getRandomArrayItem(modeArray);
 }
 
+/**
+ * Returns a random BTD6 hero.
+ * @param  None
+ * @return {String}     - Name of a random hero.
+ */
 function getRandomHero() {
     return random.getRandomObjectKey(BTD6Data.btd6_heroes_object);
 }
 
+/**
+ * Returns a collection of random towers.
+ * @param  {Number}   noOfTowers  - Positive integer that is the number of random towers wanted. Note that there are a maximum of 22 towers, thus anything more than 22 makes no sense.
+ * @param  {String}   [towerType] - Optional argument of either "primary", "military", "magic" or "support". If a type is specified, then only towers of that type will be randomly chosen.
+ * @return {String[]}             - Array of tower names.
+ */
 function getRandomTowers(noOfTowers, towerType) {
     let towerArray = [];
 
