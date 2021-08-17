e1afb16dc46a69dc64982924f97f1d22b3beb97e
maquarocket <76931815+maquarocket@users.noreply.github.com>
2021-08-17 22:01:38 +0800

    Refactored PlayerField component.
    PlayerField component is now a standalone component instead of a nested component in a function that is in a component.

--- src/js/components/Results.js
+++ src/js/components/Results.js
@@ -3,32 +3,12 @@ import React from 'react';
 import TextBTD6Font from './TextBTD6Font';
 import BlackBorder from './BlackBorder';
 import ResultsCard from './ResultsCard';
+import PlayerField from './PlayerField';
 
 
 function Results(props) {
 
     const random_player_choices = (heroes, towers) => {
-        console.log(heroes);
-        const PlayerField = (props) => {
-
-            const TowerListItem = (props) => {
-                return <TextBTD6Font tag={"li"} className="list-group-item">Tower {props.towerNo}: {props.tower}</TextBTD6Font>;
-            }
-
-            const towers = [];
-            for (let i = 1; i <= props.towers.length; i++) {
-                towers.push(<TowerListItem towerNo={i} tower={props.towers[i-1]} />);
-            }
-            return (
-                <ResultsCard title={"Player ".concat(props.playerNo)}>
-                    <ul className="list-group list-group flush">
-                        {props.hero.length > 0 && <TextBTD6Font tag={"li"} className="list-group-item">Hero: {props.hero}</TextBTD6Font>}
-                        {props.towers.length > 0 && towers}
-                    </ul>
-                </ResultsCard>
-            )
-        }
-
         const choices = [];
         const playerCount = heroes.length;
         const towerCount = towers.length / heroes.length;
