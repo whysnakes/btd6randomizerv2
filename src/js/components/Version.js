79d4efe4f0d0c4cedbdcfb69ca3f5f7f4be0bf52
whysnakes <88395238+whysnakes@users.noreply.github.com>
2021-08-18 01:20:55 +0800

    Update changelog link in footer
    Changed from relative link to fixed link (relative link was not working)

--- src/js/components/Version.js
+++ src/js/components/Version.js
@@ -5,7 +5,7 @@ import { btd6_version, randomizer_version } from '../btd6info.js';
 function Version() {
     return (
         <div className="transbox w-100 fixed-bottom text-end">
-            <a href="../../CHANGELOG.md" id="pagefooter" className="cleanlink mx-2">
+            <a href="https://github.com/whysnakes/btd6randomizerv2/blob/main/CHANGELOG.md" id="pagefooter" className="cleanlink mx-2">
                 Version <strong>{randomizer_version}</strong> based on BTD6 version <strong>{btd6_version}</strong>
             </a>
         </div>
