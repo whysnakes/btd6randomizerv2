657e3544ed70f55ec7b0fe0680fe220f94e523a3
maquarocket <76931815+maquarocket@users.noreply.github.com>
2021-08-21 14:22:51 +0800

    Added PropTypes to BlackBorder component.

--- src/js/components/BlackBorder.js
+++ src/js/components/BlackBorder.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 
 function BlackBorder(props) {
@@ -7,6 +8,10 @@ function BlackBorder(props) {
             {props.children}
         </div>
     )
+}
+
+BlackBorder.propTypes = {
+    children: PropTypes.node
 }
 
 
