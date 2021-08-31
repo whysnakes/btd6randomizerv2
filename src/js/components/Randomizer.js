a9e169b614028d5bafffdeb2b78655b1845fa434
whysnakes <88395238+whysnakes@users.noreply.github.com>
2021-08-31 18:07:37 +0800

    Wrapped some options in Rows and Cols based on Bootstrap guidelines
    For the Players form slider, added a Row to allow it to offset with other options
    For the "This option prevents..." form label, made the Col component separate to allow further styling on the label itself

--- src/js/components/Randomizer.js
+++ src/js/components/Randomizer.js
@@ -74,11 +74,14 @@ function Randomizer() {
                 <Col>
                     <div className="Options">
                         <OptionsFormContainer onSubmit={generateRandomOptions}>
-
-                            <CustomFormSlider className="col-sm-11 offset-sm-1 mb-3" controlId="player_count" min="1" max="4" value={playerCount} onChange={({target:{value}}) => setPlayerCount(value)}>
-                                Number of players:
-                            </CustomFormSlider>
-
+                            
+                            {/* offset this option to align with other options */}
+                            <Row>
+                                <CustomFormSlider className="col-sm-11 offset-sm-1 mb-3" controlId="player_count" min="1" max="4" value={playerCount} onChange={({target:{value}}) => setPlayerCount(value)}>
+                                    Number of players:
+                                </CustomFormSlider>
+                            </Row>
+                            
                             <CustomFormSwitch className="mb-3" checked={isRandomizeMap} onChange={() => setRandomizeMap(!(isRandomizeMap))}>
                                 Randomize Map?
                             </CustomFormSwitch>
@@ -95,10 +98,12 @@ function Randomizer() {
                                 <CustomFormSwitch checked={isRandomizeTowers} onChange={() => setRandomizeTowers(!(isRandomizeTowers))}>
                                     Randomize Towers?
                                 </CustomFormSwitch>
-                                <Form.Text muted className="text-start ms-5">
-                                    This option prevents <strong>X Monkeys Only</strong> modes from being picked.
-                                </Form.Text>
-                                <CustomFormSwitch className="ms-5" checked={isRestrictTowerType} onChange={() => setRestrictTowerType(!(isRestrictTowerType))} disabled={!(isRandomizeTowers)}>
+                                <Col sm={{span: 11, offset: 1}}>
+                                    <Form.Text muted className="text-start ms-2">
+                                        This option prevents <strong>X Monkeys Only</strong> modes from being picked.
+                                    </Form.Text>
+                                </Col>
+                                <CustomFormSwitch className="col-sm-11 offset-sm-1" checked={isRestrictTowerType} onChange={() => setRestrictTowerType(!(isRestrictTowerType))} disabled={!(isRandomizeTowers)}>
                                     Restrict each player to one tower type?
                                 </CustomFormSwitch>
                                 <CustomFormSlider className="mb-3 col-sm-11 offset-sm-1" controlId="tower_count" min="1" max="4" disabled={!(isRandomizeTowers)} value={maxTowers} onChange={({target:{value}}) => setMaxTowers(value)}>
